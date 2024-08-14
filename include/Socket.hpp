84e579502c8d4eed4673e586c3698af816ac0a2b
SlothsAreLazyTho <chinobijman@gmail.com>
2024-08-14 15:13:47 +0200

    Minor format mistake

--- include/Socket.hpp
+++ include/Socket.hpp
@@ -1,65 +1,54 @@
 #pragma once
 
-#include <string>
 #include <cstring>
 #include <iostream>
+#include <string>
 
+#include <fcntl.h>
+#include <poll.h>
 #include <stdbool.h>
 #include <unistd.h>
-#include <poll.h>
-#include <fcntl.h>
 
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
 
 #include <exception>
 #include <vector>
-
-/***
- * Maybe put it in a struct? Like here?
-*/
-#ifdef DEBUG
-	typedef struct
-	{
-		struct pollfd	fd;
-		struct addrinfo	_hints;
-		struct addrinfo	*addrinfo;
-	}	socket_t;
-#endif
 
 #define SERVER 1
 #define CLIENT 2
 
 class Socket
 {
-	private:
-		struct addrinfo	_hints;
+  private:
+	struct addrinfo _hints;
 
-	protected:
-		struct pollfd	fd;
-		struct addrinfo	*addrinfo;
-		std::string		_ip;
-		std::string		_port;
-		
-		int	_open;
+  protected:
+	struct pollfd fd;
+	struct addrinfo *addrinfo;
+	std::string _ip;
+	std::string _port;
 
-		virtual int open(const char *addr, const char *port, int type);
-		
-		void setConnected(int open);
-	public:
-		Socket();
-		virtual ~Socket();
+	int _open;
 
-		void close(void);
+	virtual int open(const char *addr, const char *port, int type);
 
-		const std::string& getHost() const;
-		const std::string& getPort() const;
+	void setConnected(int open);
 
-		int isConnected(void);
-		int isClosed(void);
-		int getSocket(void);
+  public:
+	Socket();
+	virtual ~Socket();
 
-		struct addrinfo *getAddress(void);
-		struct pollfd	getFileDescriptor();
+	void close(void);
+
+	const std::string &getHost() const;
+	const std::string &getPort() const;
+
+	int isConnected(void);
+	int isClosed(void);
+	int getSocket(void);
+
+	struct addrinfo *getAddress(void);
+	struct pollfd getFileDescriptor();
 };
